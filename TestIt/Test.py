e8da9fd1762d697e5dc893f668df467b3a80837b
Elia Menoni <menonielia00@gmail.com>
2024-02-13 22:16:18 +0100

    Typo correction

--- TestIt/Test.py
+++ TestIt/Test.py
@@ -15,18 +15,18 @@ env = jinja2.Environment(loader=jinja2.loaders.FileSystemLoader('./templates'),
 template = env.get_template("base.tex")
 
 data = ex.to_dict()
-test = template.render(data=data)
-corrected = template.render(data=data, correction=True)
+student = template.render(data=data)
+teacher = template.render({"data": data, "correction": True})
 
 compiled_file_name = data['heading'].replace('-', '').replace(' ', '_')
 
 # COMPILATION
 # Students
 with open('output/' + compiled_file_name + '_student.tex', 'w') as out:
-    out.write(test)
+    out.write(student)
 # Teacher
 with open('output/' + compiled_file_name + '_teacher.tex', 'w') as out:
-    out.write(test)
+    out.write(teacher)
 
 pwd = os.getcwd()
 os.system(f"pdflatex -shell-escape -interaction=nonstopmode -output-directory={pwd}/output {pwd}/output/{compiled_file_name}_student.tex")
